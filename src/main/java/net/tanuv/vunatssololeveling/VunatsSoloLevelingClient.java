9bc1c60687356c58a8e5f17d24de3c802a8a8b9d
Tanuv Nair <tanuvnair@gmail.com>
2024-06-06 07:50:05 +0530

    Basic setup done

---
+++ src/main/java/net/tanuv/vunatssololeveling/VunatsSoloLevelingClient.java
@@ -0,0 +1,10 @@
+package net.tanuv.vunatssololeveling;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VunatsSoloLevelingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
